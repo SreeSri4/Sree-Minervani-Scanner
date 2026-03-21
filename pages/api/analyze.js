b89258b7bfac7ac9bca97ed536d29a58134dde33
SreeSri4 <101240173+SreeSri4@users.noreply.github.com>
2026-03-21 21:48:46 +0530

    Update analyze.js

--- pages/api/analyze.js
+++ pages/api/analyze.js
@@ -96,7 +96,7 @@ async function fetchYahooData(symbol) {
 
       const result = json?.chart?.result?.[0];
       if (!result) { lastError = new Error(`Empty response from ${host}`); continue; }
-      const url2 = `https://groww.in/v1/api/search/v3/query/global/st_query?entity_type=stocks&from=0&query=${encodeURIComponent(symbol)}&size=6&web=true`
+      const url2 = `https://groww.in/v1/api/search/v3/query/global/st_query?entity_type=stocks&from=0&query=${encodeURIComponent(symbol)}&size=6&web=true`;
       const res1 = await fetch(url2);
       const groww_id = res1?.content?[0]?.id;
       return parseYahooResult(result, symbol,groww_id);
